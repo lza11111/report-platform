8ea0b8f15b5d20d347aba61a6e9ff7d587ff68ee
unknown <liuzhian@cn.net.ntes>
2019-01-23 20:46:44 +0800

    add description field

--- src/main/java/com/netease/music/platform/report/Model/ReportTemplate.java
+++ src/main/java/com/netease/music/platform/report/Model/ReportTemplate.java
@@ -10,6 +10,10 @@ public class ReportTemplate {
 
     private String user;
 
+    private String title;
+
+    private String description;
+
     private JSONObject content;
 
     private Date createTime;
@@ -30,6 +34,22 @@ public class ReportTemplate {
 
     public void setUser(String user) {
         this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public JSONObject getContent() {
